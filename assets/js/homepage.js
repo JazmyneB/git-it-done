5eee42e98f193e88960faa9d5d065c0f15a805f0
JazmyneB <bradleyj1@email.arizona.edu>
2021-06-09 10:59:55 -0700

    search by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 
 var getUserRepos = function(user) {
@@ -86,6 +87,38 @@ var displayRepos = function(repos, searchTerm) {
 }
 
 };
+
+var getFeaturedRepos = function(language){
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response){
+    if (response.ok){
+      response.json().then(function(data){
+        //console.log(data);
+        displayRepos(data.items, language);
+      });
+      //console.log(response);
+    } else{
+      alert('Error: Github User Not Found');
+    }
+  });
+
+
+};
+
+var buttonClickHandler = function(event){
+  var language = event.target.getAttribute("data-language");
+  if (language){
+    getFeaturedRepos(language);
+
+    //clear old content
+    repoContainerEl.textContent = "";
+  }
+  //console.log(language);
+  //getFeaturedRepos(language);
+
+}
   
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
